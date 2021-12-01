5fa91eb66ff20362ec86b0d12dd9dae619c6673d
MRQuinteroGordon <60995506+MRQuinteroGordon@users.noreply.github.com>
2021-11-30 23:59:09 -0700

    laptop changes

--- src/main/Anomalous.java
+++ src/main/Anomalous.java
@@ -48,13 +48,13 @@ public class Anomalous extends JFrame {
 	 * Create the frame.
 	 */
 	public Anomalous() {
-		setIconImage(Toolkit.getDefaultToolkit().getImage(Anomalous.class.getResource("/images/logo.png")));
+		//setIconImage(Toolkit.getDefaultToolkit().getImage(main.Anomalous.class.getResource("/anomalous/images/logo.png")));
 		setFont(new Font("Nineteen Ninety Three", Font.PLAIN, 12));
 		setTitle("Anomalous.exe");
 		setBackground(Color.RED);
 		setForeground(new Color(0, 0, 255));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 462, 619);
+		setBounds(100, 100, 480, 635);
 		contentPane = new JPanel();
 		contentPane.setBackground(UIManager.getColor("Button.background"));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -67,44 +67,10 @@ public class Anomalous extends JFrame {
 		separator.setBounds(0, 257, 440, 7);
 		contentPane.add(separator);
 		
-		JTextPane txtpnWelcomeTo = new JTextPane();
-		txtpnWelcomeTo.setBackground(UIManager.getColor("Button.background"));
-		txtpnWelcomeTo.setEditable(false);
-		txtpnWelcomeTo.setFont(new Font("Nineteen Ninety Seven", Font.PLAIN, 20));
-		txtpnWelcomeTo.setText("Welcome to");
-		txtpnWelcomeTo.setBounds(153, 11, 156, 34);
-		contentPane.add(txtpnWelcomeTo);
-		
 		JSeparator separator_1 = new JSeparator();
 		separator_1.setBackground(new Color(204, 204, 204));
 		separator_1.setBounds(0, 511, 450, 7);
 		contentPane.add(separator_1);
-		
-		JTextPane txtpnAnomalous = new JTextPane();
-		txtpnAnomalous.setBackground(UIManager.getColor("Button.background"));
-		txtpnAnomalous.setEditable(false);
-		txtpnAnomalous.setForeground(new Color(255, 204, 51));
-		txtpnAnomalous.setFont(new Font("A Goblin Appears!", Font.BOLD, 42));
-		txtpnAnomalous.setText("ANOMALOUS");
-		txtpnAnomalous.setBounds(11, 79, 440, 48);
-		contentPane.add(txtpnAnomalous);
-		
-		JTextPane txtpnAnInteractiveFiction = new JTextPane();
-		txtpnAnInteractiveFiction.setBackground(UIManager.getColor("Button.background"));
-		txtpnAnInteractiveFiction.setEditable(false);
-		txtpnAnInteractiveFiction.setText("An Interactive Fiction Story");
-		txtpnAnInteractiveFiction.setFont(new Font("Nineteen Ninety Seven", Font.PLAIN, 20));
-		txtpnAnInteractiveFiction.setBounds(33, 142, 396, 34);
-		contentPane.add(txtpnAnInteractiveFiction);
-		
-		JTextPane txtpnWithATwist = new JTextPane();
-		txtpnWithATwist.setBackground(UIManager.getColor("Button.background"));
-		txtpnWithATwist.setEditable(false);
-		txtpnWithATwist.setText("with a twist!");
-		txtpnWithATwist.setForeground(new Color(204, 0, 0));
-		txtpnWithATwist.setFont(new Font("Nineteen Ninety Seven", Font.ITALIC, 24));
-		txtpnWithATwist.setBounds(104, 187, 254, 39);
-		contentPane.add(txtpnWithATwist);
 		
 		JTextPane txtpnPleaseEnterYour = new JTextPane();
 		txtpnPleaseEnterYour.setBackground(UIManager.getColor("Button.background"));
@@ -119,7 +85,7 @@ public class Anomalous extends JFrame {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				String playername = nametxt.getText();
-				//JOptionPane.showMessageDialog(null, "Henlo Fren");
+				JOptionPane.showMessageDialog(null, "Henlo Fren");
 			}
 		});
 		start_btn.setFont(new Font("Nineteen Ninety Seven", Font.BOLD, 20));
@@ -131,5 +97,44 @@ public class Anomalous extends JFrame {
 		nametxt.setBounds(121, 333, 219, 38);
 		contentPane.add(nametxt);
 		nametxt.setColumns(10);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 11, 468, 235);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JTextPane txtpnWelcomeTo = new JTextPane();
+		txtpnWelcomeTo.setBounds(156, 29, 156, 34);
+		panel.add(txtpnWelcomeTo);
+		txtpnWelcomeTo.setBackground(UIManager.getColor("Button.background"));
+		txtpnWelcomeTo.setEditable(false);
+		txtpnWelcomeTo.setFont(new Font("Nineteen Ninety Seven", Font.PLAIN, 20));
+		txtpnWelcomeTo.setText("Welcome to");
+		
+		JTextPane txtpnAnomalous = new JTextPane();
+		txtpnAnomalous.setBounds(14, 74, 440, 48);
+		panel.add(txtpnAnomalous);
+		txtpnAnomalous.setBackground(UIManager.getColor("Button.background"));
+		txtpnAnomalous.setEditable(false);
+		txtpnAnomalous.setForeground(new Color(255, 204, 51));
+		txtpnAnomalous.setFont(new Font("A Goblin Appears!", Font.BOLD, 42));
+		txtpnAnomalous.setText("ANOMALOUS");
+		
+		JTextPane txtpnAnInteractiveFiction = new JTextPane();
+		txtpnAnInteractiveFiction.setBounds(36, 155, 396, 34);
+		panel.add(txtpnAnInteractiveFiction);
+		txtpnAnInteractiveFiction.setBackground(UIManager.getColor("Button.background"));
+		txtpnAnInteractiveFiction.setEditable(false);
+		txtpnAnInteractiveFiction.setText("An Interactive Fiction Story");
+		txtpnAnInteractiveFiction.setFont(new Font("Nineteen Ninety Seven", Font.PLAIN, 20));
+		
+		JTextPane txtpnWithATwist = new JTextPane();
+		txtpnWithATwist.setBounds(131, 185, 205, 39);
+		panel.add(txtpnWithATwist);
+		txtpnWithATwist.setBackground(UIManager.getColor("Button.background"));
+		txtpnWithATwist.setEditable(false);
+		txtpnWithATwist.setText("with a twist!");
+		txtpnWithATwist.setForeground(new Color(204, 0, 0));
+		txtpnWithATwist.setFont(new Font("Nineteen Ninety Seven", Font.ITALIC, 24));
 	}
 }
